05b4e24502162d412b97109355e146b5c7adb756
Yaokeng Chen <kenchan2048sk@gmail.com>
2022-10-24 18:48:29 +0800

    static demo 1 final

--- staticDemo/src/a1StaticDemo/Student.java
+++ staticDemo/src/a1StaticDemo/Student.java
@@ -2,13 +2,14 @@ package a1StaticDemo;
 
 public class Student {
     //属性：姓名 年龄 性别
+    //新增：老师的姓名
     private String name;
     private int age;
     private String gender;
-
+    public static String teacherName; //所有 student 类的对象都共享同一个teacherName
     public Student() {
     }
-
+    //ALT+Insert 可以直接插入构造方法和get，set
     public Student(String name, int age, String gender) {
         this.name = name;
         this.age = age;
@@ -45,6 +46,6 @@ public class Student {
     }
 
     public void show(){
-        System.out.println(name + ", " + age + ", " + gender);
+        System.out.println(name + ", " + age + ", " + gender +", " + teacherName);
     }
 }
