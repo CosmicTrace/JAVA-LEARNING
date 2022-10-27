4f475e8e40bd09c8ed63aa457cf42bcc1a37c364
Yaokeng Chen <kenchan2048sk@gmail.com>
2022-10-27 17:48:27 +0800

    add demo 4 main

---
+++ staticDemo/src/javafundation/a4staticdemo/Student.java
@@ -0,0 +1,37 @@
+package javafundation.a4staticdemo;
+/*
+    总结：
+        静态方法没有this关键字
+        静态方法中只能访问静态
+        非静态方法可以访问所有
+ */
+public class Student {
+    String name;
+    int age;
+    static String teacherName;
+
+    //非静态成员变量的形参中有一个隐藏的this
+    //this: 表示当前方法调用者的地址值
+    //这个this：是由虚拟机赋值的
+    public void show1(Student this){
+        System.out.println("this: "+this);
+        System.out.println(this.name + ", " + this.age + ", " + teacherName);
+
+        //调用其它方法
+        this.show2();
+        this.method();
+    }
+
+
+
+    public void show2(){
+        System.out.println("show2");
+    }
+
+    //共享则没有this
+    public static void method(){
+        //this.show1();
+        //System.out.println(this.name)
+        System.out.println("静态方法");
+    }
+}
---
+++ staticDemo/src/javafundation/a4staticdemo/StudentTest.java
@@ -0,0 +1,21 @@
+package javafundation.a4staticdemo;
+
+public class StudentTest {
+    public static void main(String[] args){
+        Student.teacherName = "阿伟老师";
+
+        Student s1 = new Student();
+        System.out.println("s1: " + s1);
+        s1.name = "张山";
+        s1.age = 23;
+        s1.show1();
+
+        System.out.println("================================================");
+
+        Student s2 = new Student();
+        System.out.println("s2: " + s2);
+        s2.name = "李四";
+        s2.age = 21;
+        s2.show1();
+    }
+}
