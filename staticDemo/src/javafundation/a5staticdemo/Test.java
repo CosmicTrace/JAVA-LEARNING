8dc14b9a4e194835c72d0dba7a91f13cd6060717
Yaokeng Chen <kenchan2048sk@gmail.com>
2022-10-27 18:31:55 +0800

    add static demo 5 main

---
+++ staticDemo/src/javafundation/a5staticdemo/Test.java
@@ -0,0 +1,14 @@
+package javafundation.a5staticdemo;
+
+public class Test {
+    public static void main(String[] args){
+        //[]： 数组
+        //String: 数据类型
+        //args: 数组名
+
+        System.out.println(args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args遍历 " + args[i]);
+        }
+    }
+}
